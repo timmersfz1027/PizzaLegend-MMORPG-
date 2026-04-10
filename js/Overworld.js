75dd54fe949b23b046f467cd9f119cfeec44e888
Timbsz <79558895+timmersfz1027@users.noreply.github.com>
2026-04-10 09:04:29 +0800

    Add files via upload

---
+++ js/Overworld.js
@@ -0,0 +1,60 @@
+class Overworld {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+      //Clear off the canvas
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // establish the camera person (for now, just the hero)
+      const cameraPerson = this.map.gameObjects.hero; // this.map.gameObjects.hero;
+
+      //Update all objects, including camera person
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.update({
+          arrow: this.directionInput.direction,
+          map: this.map,
+        }); //Update the object
+      });
+
+      //Draw the lower layer
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+      //Draw the game objects
+      Object.values(this.map.gameObjects)
+        .sort((a, b) => {
+          return a.y - b.y;
+        })
+        .forEach((object) => {
+          object.sprite.draw(this.ctx, cameraPerson);
+        });
+
+      //Draw the Upper layer
+      this.map.drawUpperImage(this.ctx, cameraPerson);
+      requestAnimationFrame(() => {
+        step();
+      });
+    };
+    step();
+  }
+
+  init() {
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.map.mountObjects();
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
+    this.startGameLoop();
+
+    this.map.startCutscene([
+      { who: "hero", type: "walk", direction: "down" },
+      { who: "hero", type: "walk", direction: "down" },
+      { who: "npcA", type: "walk", direction: "left" },
+      { who: "npcA", type: "walk", direction: "left" },
+      { who: "npcA", type: "stand", direction: "up", time: 800 },
+    ]);
+  }
+}
